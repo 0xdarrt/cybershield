9543738314867285dad9750a478b5fdd1e070588
0xdaart <harshitpro29@gmail.com>
2025-12-06 01:00:40 +0530

    Update Layout.jsx

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,1 +1,15 @@
 
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+
+export default function Layout() {
+  return (
+    <div className="min-h-screen bg-gray-950 text-gray-100">
+      <Navbar />
+      <main className="pt-20 px-4">
+        <Outlet />
+      </main>
+    </div>
+  );
+}
